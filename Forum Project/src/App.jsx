c41478228d038c0eb347516a0c2e037818907ad5
Plamen <plamen.m.g.2005@gmail.com>
2025-02-13 16:15:33 +0200

    Installing node files in the right place

--- Forum Project/src/App.jsx
+++ Forum Project/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   const [appState, setAppState] = useState({
@@ -10,27 +10,30 @@ function App() {
   });
 
   const onLogout = () => {
-    logoutUser()
-      .then(() => {
-        setAppState({
-          user: null,
-          userData: null,
-        });
+    logoutUser().then(() => {
+      setAppState({
+        user: null,
+        userData: null,
       });
+    });
   };
 
   return (
-    <AppContext.Provider value={{...appState, setContext: setAppState}}>
+    <AppContext.Provider value={{ ...appState, setContext: setAppState }}>
       <BrowserRouter>
         <div className="App">
-        <Link to='/'>Home</Link> &nbsp;
-      {user === null && <Link to='/register'>Register</Link>} &nbsp;
-      {user === null && <Link to='/login'>Login</Link>} &nbsp;
-      {user !== null && <Link to='/' onClick={onLogout}>Logout</Link>}
+          <Link to="/">Home</Link> &nbsp;
+          {user === null && <Link to="/register">Register</Link>} &nbsp;
+          {user === null && <Link to="/login">Login</Link>} &nbsp;
+          {user !== null && (
+            <Link to="/" onClick={onLogout}>
+              Logout
+            </Link>
+          )}
         </div>
       </BrowserRouter>
     </AppContext.Provider>
   );
 }
 
-export default App
+export default App;
