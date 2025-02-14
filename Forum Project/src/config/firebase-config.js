551229910e61d1dfd77a582892dfd1b1ee4f0c20
Plamen <plamen.m.g.2005@gmail.com>
2025-02-14 13:22:27 +0200

    Add NotFound and Home components, implement AppContext, update routing,implement register function

--- Forum Project/src/config/firebase-config.js
+++ Forum Project/src/config/firebase-config.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDFbe_fq0T_9Ve1jEv_TbZ9xb-yXbaWrBU",
-  authDomain: "forum-64b81.firebaseapp.com",
-  projectId: "forum-64b81",
-  storageBucket: "forum-64b81.firebasestorage.app",
-  messagingSenderId: "1001058422129",
-  appId: "1:1001058422129:web:6fb09e104639d205053ad0",
-  measurementId: "G-64WRXCQ5HG",
-  databaseURL: "https://forum-64b81-default-rtdb.europe-west1.firebasedatabase.app/"
-};
+  authDomain: "a67-not-a-twitter.firebaseapp.com",
+  projectId: "a67-not-a-twitter",
+  storageBucket: "a67-not-a-twitter.firebasestorage.app",
+  messagingSenderId: "458931369831",
+  appId: "1:458931369831:web:fca31a09791d7173f5a1c2",
+  databaseURL: "https://forum-64b81-default-rtdb.europe-west1.firebasedatabase.app/"};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
