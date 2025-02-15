ebe2a27775104d9f78a20374b5ac1ccc22e6842d
Preslav Stankov <preslav.stankov@gmail.com>
2025-02-15 22:37:06 +0200

    Saving web tokens for accounts and avatar image.

--- Forum Project/src/App.jsx
+++ Forum Project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './components/Register/Register';
 import Home from './view/Home/Home';
@@ -7,9 +7,12 @@ import Login from './components/Login/Login';
 import './App.css';
 import Header from './components/Header/Header';
 import { AppContext } from './components/store/app.context';
-import TrendingView from './components/Dashboard/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 import PostView from './components/PostView/PostView';
 import UploadView from './components/Upload/Upload';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './config/firebase-config';
+import { getUserData } from './services/users.service';
 
 function App() {
   const [appState, setAppState] = useState({
@@ -17,14 +20,34 @@ function App() {
     userData: null,
   });
 
-  const onLogout = () => {
-    logoutUser().then(() => {
-      setAppState({
-        user: null,
-        userData: null,
-      });
+  const [user, loading, error] = useAuthState(auth);
+
+  if (appState.user !== user) {
+    setAppState({
+      ...appState,
+      user,
     });
-  };
+  }
+
+  useEffect(() => {
+    if (!user) return;
+
+    getUserData(appState.user.uid)
+      .then((data) => {
+        const userData = data[Object.keys(data)[0]];
+        setAppState({
+          ...appState,
+          userData,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [user]);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <BrowserRouter>
@@ -32,7 +55,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/trending" element={<TrendingView />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/post" element={<PostView />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register></Register>} />
--- Forum Project/src/components/Header/Header.jsx
+++ Forum Project/src/components/Header/Header.jsx
@@ -1,43 +1,52 @@
-import { NavLink } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { AppContext } from "../store/app.context";
-import { logoutUser } from "../../services/auth.service";
-import "./Header.css";
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../store/app.context';
+import { logoutUser } from '../../services/auth.service';
+import './Header.css';
+import { getUserData } from '../../services/users.service';
 
 export default function Header() {
+  const { user, setAppState } = useContext(AppContext);
+  const [userData, setUserData] = useState({});
+  const navigate = useNavigate();
 
-    const { user, userData, setAppState } = useContext(AppContext);
-    const navigate = useNavigate();
-  
-    const logout = () => {
-      logoutUser()
-        .then(() => {
-          setAppState({
-            user: null,
-            userData: null
-          });
-          navigate('/login');
-        })
-        .catch((error) => {
-          console.error(error.message);
-        })
-    }
-
-
-    return (
-        <div>
-            <h1>Forum</h1>
-            <div>
-                
-                <NavLink to="/trending">Dashboard</NavLink> |&nbsp;
-                {!user && (<>
-                <NavLink to="/login">Login</NavLink> |&nbsp;
-                <NavLink to="/register">Register</NavLink>
-                </>)}
-                {user && <NavLink to="/upload">Upload</NavLink>}
-                {user && <button onClick={logout}>Logout</button>}
-            </div>
-        </div>
+  useEffect(() => {
+    getUserData(user.uid).then((data) =>
+      setUserData(data[Object.keys(data)[0]])
     );
+  }, [user]);
+
+  const logout = () => {
+    logoutUser()
+      .then(() => {
+        setAppState({
+          user: null,
+          userData: null,
+        });
+        navigate('/login');
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Forum</h1>
+      {user && <img src={userData.avatarUrl} alt="avatar" />}
+      <div>
+        <NavLink to="/">Home</NavLink> |&nbsp;
+        <NavLink to="/dashboard">Dashboard</NavLink> |&nbsp;
+        {!user && (
+          <>
+            <NavLink to="/login">Login</NavLink> |&nbsp;
+            <NavLink to="/register">Register</NavLink>
+          </>
+        )}
+        {user && <NavLink to="/upload">Upload</NavLink>}
+        {user && <button onClick={logout}>Logout</button>}
+      </div>
+    </div>
+  );
 }
