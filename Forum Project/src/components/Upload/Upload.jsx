b4d8430e2cfe5bc609d981964f6b314d435cf5b7
Preslav Stankov <preslav.stankov@gmail.com>
2025-02-17 01:15:57 +0200

    feat: Add empty posts.service.js; update Upload.jsx to push posts in DB.

--- Forum Project/src/components/Upload/Upload.jsx
+++ Forum Project/src/components/Upload/Upload.jsx
@@ -1,64 +1,94 @@
-import { useState, useContext } from 'react';
-import { AppContext } from '../../store/app.context';
-import { db }  from '../../config/firebase-config';
+import { useState, useContext, useEffect } from 'react';
+import { AppContext } from '../store/app.context';
+import { db } from '../../config/firebase-config';
 import { ref, set, push } from 'firebase/database';
+import { getUserData } from '../../services/users.service';
 
+const UploadView = () => {
+  const { user } = useContext(AppContext);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [userHandle, setUserHandle] = useState({});
 
-const UploadView = () => {  
-    const { user } = useContext(AppContext);
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+  useEffect(() => {
+    getUserData(user.uid).then((data) =>
+      setUserHandle(data[Object.keys(data)[0]])
+    );
+  }, []);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Post submitted:', {title, content});
-        setTitle('');
-        setContent('');
-    };
-
-    if(!user) {
-        return alert("You must be logged in to upload a post");
-    }
-    
-    const post = {
-        title,
-        content,
-        author: user.uid,
-        createdOn: new Date().toString(),
-    };
-
+  const postsDB = async () => {
     try {
-        const newPostRef = push(ref(db, 'posts'));
-        set(newPostRef, post);
-        setTitle('');
-        setContent('');
-        alert('Post uploaded successfully');
-    }   catch (error) {
-        console.error('Error uploading post:', error.message);
-        alert('Error uploading post');
-    }
-    
-    
-    
+      const newPostRef = push(ref(db, 'posts'));
+      const postId = newPostRef.key;
 
-    return (
-        <div>
+      await Promise.all([
+        set(ref(db, `posts/${postId}`), post),
+        set(ref(db, `users/${userHandle.handle}/posts/${postId}`), post),
+      ]);
+
+      setTitle('');
+      setContent('');
+      alert('Post uploaded successfully');
+    } catch (error) {
+      console.error('Error uploading post:', error.message);
+      alert('Error uploading post');
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let isValid = true;
+
+    if (!title || !content) {
+      alert(`Please enter title and content`);
+      isValid = false;
+    }
+    if (title.length < 16 || title.length > 64) {
+      alert(`The title must be between 16 and 64 symbols.`);
+      isValid = false;
+    }
+    if (content.length < 32 || content.length > 8192) {
+      alert(`The content must be between 32 symbols and 8192 symbols.`);
+      isValid = false;
+    }
+
+    if (isValid) {
+      console.log('Post submitted:', { title, content });
+      postsDB();
+    }
+  };
+
+  if (!user) {
+    return alert('You must be logged in to upload a post');
+  }
+
+  const post = {
+    title,
+    content,
+    author: user.uid,
+    createdOn: new Date().toString(),
+  };
+
+  return (
+    <div>
       <h2>Upload a Post</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="title">Title:</label>
+          <label htmlFor="title">Title: </label>
           <input
             type="text"
             id="title"
+            name="title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             required
           />
         </div>
         <div>
-          <label htmlFor="content">Content:</label>
+          <label htmlFor="content">Content: </label>
           <textarea
             id="content"
+            name="content"
             value={content}
             onChange={(e) => setContent(e.target.value)}
             required
@@ -69,6 +99,5 @@ const UploadView = () => {
     </div>
   );
 };
-
 
 export default UploadView;
---
+++ Forum Project/src/services/posts.service.js
@@ -0,0 +1,3 @@
+import { get, set, ref, query, equalTo, orderByChild } from 'firebase/database';
+import { db } from '../config/firebase-config';
+
